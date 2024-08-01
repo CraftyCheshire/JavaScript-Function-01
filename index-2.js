9a8d6bf242aced6a1c7459e3dcea8b80cf0cbcfe
craftycheshire <mini_vann@ymail.com>
2024-07-31 21:48:44 -0500

    second song

---
+++ index-2.js
@@ -0,0 +1,77 @@
+console.log("Hello 2")
+
+function chorus1() {
+    let words = "";
+    words += "I'm in the mood for a melody\n"
+    words += "I'm in the mood for a melody\n"
+    words += "I'm in the mood"
+    return words;
+}
+
+function chorus2() {
+    let words = "";
+    words += "I can make you dance\n"
+    words += "I can make you sing\n"
+    words += "I can make you dance\n"
+    words += "I can make you sing\n"
+    words += "If you want me to"
+    return words;
+}
+
+function chorus3() {
+    let words = "";
+    words += "Any little song that you wanna to sing\n"
+    words += "Any little song that you wanna to sing"
+    return words;
+}
+
+function line1() {
+    let words = "";
+    words += "I'm in the mood"
+    return words;
+
+}
+
+function line2() {
+    let words = "";
+    words += "If you want me to"
+    return words;
+}
+
+a = chorus1();
+b = chorus2();
+c = chorus3();
+d = line1();
+e = line2();
+
+console.log(a);
+console.log(a);
+console.log(a);
+
+console.log(b);
+console.log("Oh,");
+console.log(b);
+console.log("Oh,");
+console.log(b);
+
+console.log(c)
+console.log("Any song will do");
+console.log(c)
+console.log("It's up to you");
+console.log(c)
+console.log("You're blue");
+
+console.log(d);
+console.log(d);
+console.log(d);
+
+console.log("I can write it on the door");
+console.log("I can put it on the floor");
+console.log("I can do anything that you want me for");
+console.log(e);
+
+console.log("Do it right, do it wrong");
+console.log("Cause a matter of fact it'll turn out to be strong");
+console.log(e);
+
+console.log(e);
---
+++ index.js
@@ -0,0 +1,57 @@
+function chorus1 (){
+    console.log("Tonight you're falling in love");
+    console.log("Let me go now");
+    console.log("This feeling's tearing me up");
+    console.log("Here we go now");
+};
+
+function chorus2 (){
+    console.log("Now if she does it like this");
+    console.log("Will you do it like that?");
+    console.log("Now if she touches like this");
+    console.log("Will you touch her right back?");
+    console.log("Now if she moves like this");
+    console.log("Will you move like that? Come on");
+}
+
+function chorus3 (){
+    console.log("Shake, shake, shake, shake, a-shake it");
+    console.log("A-shake, shake, shake, shake, a-shake it");
+    console.log("A-shake, shake, shake, shake, a-shake it");
+    console.log("A-shake, shake, shake, shake, a-shake it");
+    console.log("A-shake, shake, shake, shake, a-shake it");
+};
+
+console.log("Hello World");
+
+console.log("Let's drop");
+console.log("Yeah, come on");
+console.log("Shake, shake");
+
+console.log("I'll take you home if you don't leave me at the front door");
+console.log("(Leave me at the front door)");
+console.log("Your body's cold but girl, we're gettin' so warm");
+console.log("And I was thinking of ways that I could get inside");
+console.log("(Get inside)");
+chorus1()
+chorus2()
+chorus3()
+console.log("Your lips tremble but your eyes are in a straight stare");
+console.log("(In a straight stare)");
+console.log("We're on the bed but your clothes are laying right there");
+console.log("And I was thinking of places that I could hide");
+console.log("(That I could hide)");
+chorus1()
+chorus2()
+chorus3()
+console.log("I saw you dancing");
+console.log("And I couldn't get you off my mind");
+console.log("I could tell that you could tell");
+console.log("That I was taking my time");
+console.log("But I was thinking of ways");
+console.log("To get you staying the night");
+console.log("Your body's shaking");
+console.log("Turn me on so I can turn off the lights");
+chorus2()
+chorus3()
+chorus3() 
